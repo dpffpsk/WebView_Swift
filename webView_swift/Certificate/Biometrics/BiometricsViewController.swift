452067fe12aad514b37f2007b2b3cffa7f774235
dpffpsk <gpljw0419@gmail.com>
2022-12-05 17:45:21 +0900

    생체인증 구현 완료

--- webView_swift/Certificate/Biometrics/BiometricsViewController.swift
+++ webView_swift/Certificate/Biometrics/BiometricsViewController.swift
@@ -11,6 +11,7 @@ import LocalAuthentication
 class BiometricsViewController: BaseViewController {
 
     let biometricsView = BiometricsView()
+    var error: NSError?
     
     enum AuthenticationState {
         case login, logout
@@ -30,7 +31,8 @@ class BiometricsViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        buttonAction()
+//        buttonAction()
+        setup()
     }
  
     override func setupLayout() {
@@ -49,25 +51,52 @@ class BiometricsViewController: BaseViewController {
         self.biometricsView.loginStateButton.addTarget(self, action: #selector(loginTapped), for: .touchUpInside)
     }
     
+    func setup() {
+        self.biometricsView.loginStateButton.addTarget(self, action: #selector(loginTapped), for: .touchUpInside)
+        
+        let authContext = LAContext()
+
+        // 지원 가능한 디바이스 구분
+        guard authContext.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) else {
+            Alert().show(message: "Error : \(error?.localizedDescription ?? "Failed to authenticate")")
+            return
+        }
+        
+        print("BiometryType : \(authContext.biometryType)")
+        
+        var imageName = ""
+        switch authContext.biometryType {
+        case .faceID:
+            imageName = "Face_ID_logo"
+        case .touchID:
+            imageName = "Touch_ID_logo"
+        default:
+            print("BiometryType : none")
+        }
+        
+        biometricsView.biometricsImageView.image = UIImage(named: imageName)
+    }
+    
     @objc func loginTapped() {
+        // state login -> logout 변경(login 상태)
         if state == .login {
             state = .logout
-        } else {
+        } else { // state logout -> login 변경(logout 상태)
             let authContext = LAContext()
-            print("authContext : \(authContext.biometryType.rawValue)")
             
-            if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
-                authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "인증하기") { success, error in
-                    
+            // 지원 가능한 디바이스 구분
+            if authContext.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+                authContext.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: "인증하기") { success, error in
                     if success {
                         DispatchQueue.main.async {
                             self.state = .login
                         }
                     } else {
-                        
+                        Alert().show(message: "Error : \(error?.localizedDescription ?? "Failed to authenticate")")
                     }
-                    print("결과", success, error)
                 }
+            } else {
+                Alert().show(message: "Error : \(error?.localizedDescription ?? "Failed to authenticate")")
             }
         }
     }
