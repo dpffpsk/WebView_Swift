e1fa54955e08ab1bb388ed6531876fe267b61ce4
dpffpsk <gpljw0419@gmail.com>
2022-12-16 17:01:22 +0900

    Top, Bottom Sheet 완성(네비게이션 바 처리 개발 중)

--- webView_swift/Sheet/SheetView.swift
+++ webView_swift/Sheet/SheetView.swift
@@ -7,18 +7,24 @@
 
 import UIKit
 
-class SheetView: BaseView {
-    private var topConstraint: NSLayoutConstraint?
-    private var bottomConstraint: NSLayoutConstraint?
+enum Gubun{
+    case top
+    case bottom
+}
+
+class SheetView: UIView {
+    var topConstraint: NSLayoutConstraint?
+    var bottomConstraint: NSLayoutConstraint?
     private var leadingConstraint: NSLayoutConstraint?
     private var trailingConstraint: NSLayoutConstraint?
+    private var gubun: Gubun!
     
     private lazy var backgroundView: UIView = {
         let view = UIView()
         return view
     }()
     
-    private lazy var contentView: UIView = {
+    lazy var contentView: UIView = {
         let view = UIView()
         view.backgroundColor = .orange
         view.clipsToBounds = true
@@ -41,25 +47,28 @@ class SheetView: BaseView {
         return label
     }()
 
-    override init() {
-        super.init()
+    init(gubun: Gubun? = .top) {
+        super.init(frame: .zero)
         
+        self.gubun = gubun
         self.backgroundColor = .clear
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapView))
         self.addGestureRecognizer(tapGestureRecognizer)
+        setupLayout()
+        setupConstraints()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func setupLayout() {
+    private func setupLayout() {
         addSubview(backgroundView)
         addSubview(contentView)
         contentView.addSubview(topLabel)
     }
     
-    override func setupConstraints() {
+    private func setupConstraints() {
         backgroundView.translatesAutoresizingMaskIntoConstraints = false
         backgroundView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         backgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
@@ -67,13 +76,20 @@ class SheetView: BaseView {
         backgroundView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         
         contentView.translatesAutoresizingMaskIntoConstraints = false
-        topConstraint = contentView.topAnchor.constraint(equalTo: self.topAnchor)
-        topConstraint?.isActive = true
-        bottomConstraint = contentView.bottomAnchor.constraint(equalTo: self.topAnchor)
-        bottomConstraint?.isActive = true
+        
+        if gubun == .top {
+            topConstraint = contentView.topAnchor.constraint(equalTo: self.topAnchor)
+            bottomConstraint = contentView.bottomAnchor.constraint(equalTo: self.topAnchor)
+        } else {
+            topConstraint = contentView.topAnchor.constraint(equalTo: self.bottomAnchor)
+            bottomConstraint = contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        }
         leadingConstraint = contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10)
-        leadingConstraint?.isActive = true
         trailingConstraint = contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10)
+        
+        topConstraint?.isActive = true
+        bottomConstraint?.isActive = true
+        leadingConstraint?.isActive = true
         trailingConstraint?.isActive = true
         
         topLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -81,7 +97,7 @@ class SheetView: BaseView {
         topLabel.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
     }
     
-    func show() {
+    func showTopSheet() {
         topConstraint?.isActive = false
         bottomConstraint?.isActive = false
         
@@ -89,17 +105,12 @@ class SheetView: BaseView {
         contentView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
         UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut) {
-            self.backgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+            self.backgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.3)
             self.layoutIfNeeded()
         }
     }
-    
-    func hide() {
-        topConstraint?.isActive = false
-        bottomConstraint?.isActive = false
-        
-        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        contentView.bottomAnchor.constraint(equalTo: self.topAnchor).isActive = true
+
+    private func hideSheet() {
         
         UIView.animate(withDuration: 0.3,
                        delay: 0,
@@ -114,7 +125,31 @@ class SheetView: BaseView {
         )
     }
     
-    @objc func didTapView(_: UITapGestureRecognizer) {
-        hide()
+    @objc private func didTapView(_: UITapGestureRecognizer) {
+        topConstraint?.isActive = false
+        bottomConstraint?.isActive = false
+        
+        if gubun == .top {
+            contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+            contentView.bottomAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        } else {
+            contentView.topAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+            contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        }
+        
+        hideSheet()
+    }
+    
+    func showBottomSheet() {
+        topConstraint?.isActive = false
+        bottomConstraint?.isActive = false
+        
+        contentView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        contentView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut) {
+            self.backgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+            self.layoutIfNeeded()
+        }
     }
 }
--- webView_swift/Sheet/SheetViewController.swift
+++ webView_swift/Sheet/SheetViewController.swift
@@ -8,40 +8,79 @@
 import UIKit
 
 class SheetViewController: BaseViewController {
-    
+
     lazy var showTopSheetButton: UIButton = {
         let btn = UIButton()
         btn.setTitle("Top Sheet", for: .normal)
         btn.setTitleColor(.white, for: .normal)
         btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
         btn.backgroundColor = .systemBlue
+        btn.sizeToFit()
         return btn
+    }()
+    
+    lazy var showBottomSheetButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Bottom Sheet", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+        btn.backgroundColor = .systemBlue
+        btn.sizeToFit()
+        return btn
+    }()
+    
+    lazy var buttonStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 30
+        return stack
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setAction()
     }
     
     override func setupLayout() {
-        view.addSubview(showTopSheetButton)
+        view.addSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(showTopSheetButton)
+        buttonStackView.addArrangedSubview(showBottomSheetButton)
     }
     
     override func setupConstraints() {
-        showTopSheetButton.translatesAutoresizingMaskIntoConstraints = false
-        showTopSheetButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        showTopSheetButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-        showTopSheetButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        showTopSheetButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        buttonStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        buttonStackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        buttonStackView.heightAnchor.constraint(equalToConstant: 130).isActive = true
+        buttonStackView.widthAnchor.constraint(equalToConstant: 130).isActive = true
     }
   
     private func setAction() {
         self.showTopSheetButton.addTarget(self, action: #selector(didTapTopSheetButton), for: .touchUpInside)
+        self.showBottomSheetButton.addTarget(self, action: #selector(didTapBottomSheetButton), for: .touchUpInside)
     }
     
     @objc func didTapTopSheetButton(_: UIButton) {
         let sheetView = SheetView()
+        
+//        view.addSubview(sheetView)
+        self.navigationController?.view.addSubview(sheetView)
+        sheetView.translatesAutoresizingMaskIntoConstraints = false
+        sheetView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        sheetView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        sheetView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        sheetView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.view.layoutIfNeeded()
+        
+//        self.navigationController?.navigationBar.isHidden = true
+        sheetView.showTopSheet()
+    }
+    
+    @objc func didTapBottomSheetButton(_: UIButton) {
+        let sheetView = SheetView(gubun: .bottom)
+        
         view.addSubview(sheetView)
         sheetView.translatesAutoresizingMaskIntoConstraints = false
         sheetView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
@@ -50,7 +89,14 @@ class SheetViewController: BaseViewController {
         sheetView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         self.view.layoutIfNeeded()
         
-        self.navigationController?.navigationBar.isHidden = true
-        sheetView.show()
+        sheetView.showBottomSheet()
+    }
+
+    override func viewDidLayoutSubviews() {
+        if let lastView = self.view.subviews.last {
+            if lastView == buttonStackView {
+                self.navigationController?.navigationBar.isHidden = false
+            }
+        }
     }
 }
